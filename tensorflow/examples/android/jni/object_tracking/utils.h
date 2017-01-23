fd807e7197030ad32bcdd4de4d9445d4e5ccae20
ggfan <gfan@google.com>
2017-01-23 15:03:26 -0800

    Some clean-ups:   1) remove added common.h and put the macro into utils.h

--- tensorflow/examples/android/jni/object_tracking/utils.h
+++ tensorflow/examples/android/jni/object_tracking/utils.h
@@ -84,6 +84,11 @@ limitations under the License.
 
 #endif  // __GOOGLE__
 
+// A macro to disallow the copy constructor and operator= functions
+// This is usually placed in the private: declarations for a class.
+#define TF_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&) = delete;         \
+  void operator=(const TypeName&) = delete
 
 #ifndef MAX
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
